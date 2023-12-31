6c42056233f4794d07b7800669e6a234d4e64af3
Sheetal ts <sheetalts025@gmail.com>
2024-01-01 04:56:37 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-from src.logger import logging
+from logger import logging
+
 
 
 def error_message_detail(error,error_detail:sys):
@@ -20,5 +21,9 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message 
+ 
 
+
+
+    
     
